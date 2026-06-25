8140744398172d88351b73a45df3b5a8d31512d5
johnsoncali123 <109111228+johnsoncali123@users.noreply.github.com>
2026-06-25 19:22:40 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1691,9 +1691,9 @@ function settingsMenu()
 		if (settingsPageID == 1) then
 			addOption(7, 2, {"Counting Respawns", "Counting Cap Points", "Counting Rounds"}, set.teamTicketType)
 			if (set.lobbyName == "Mevills") then
-				addOption(30, 2, {"Use Mevills Map", "Use Skepsis Map"}, 1)
+				addOption(30, 2, {"Use Mevills Lobby", "Use Skepsis Lobby"}, 1)
 			else
-				addOption(30, 2, {"Use Mevills Map", "Use Skepsis Map"}, 2)
+				addOption(30, 2, {"Use Mevills Lobby", "Use Skepsis Lobby"}, 2)
 			end
 			if (set.teamTicketType == 3) then
 				addOption(27, 1, "Roundstart Spawn Swap", set.roundSpawnSwap)
